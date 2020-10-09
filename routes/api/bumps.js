4ceb50af86e6cfa55844353cb67199b5e24c080d
romain herry <romainherry@MacBook-Pro.local>
2020-10-09 15:58:21 -0700

    First commit on work-branch

---
+++ routes/api/bumps.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const bumpCtrl = require('../../controllers/api/bumps');
+
+// write your routes here
+
+
+
+
+
+
+
+
+module.exports = router;
