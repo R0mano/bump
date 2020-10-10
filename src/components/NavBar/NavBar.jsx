f5a279f6aad02bc250942c197fd63b9f2866a229
romain herry <romainherry@MacBook-Pro.local>
2020-10-09 21:37:23 -0700

    Add first component

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './NavBar.css';
+
+export default NavBar
+
+
+function NavBar() {
+  return(
+    <div>
+      <p>This is the NavBar</p>
+    </div>
+  )
+}
---
+++ src/pages/App/App.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import { Route } from 'react-router-dom';
+import NavBar from '../../components/NavBar/NavBar';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <NavBar/>
+      <h1>hello world</h1>
+    </div>
+  );
+}
+
+export default App;
