bd402f8782351c250cf372e6980d36ed566994da
romain herry <romainherry@MacBook-Pro.local>
2020-10-16 20:28:05 -0700

    hotfix

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -14,7 +14,7 @@ import { PromiseProvider } from "mongoose";
 // import ProfilePage from "../ProfilePage/ProfilePage";
 let socket;
 // const END_POINT = 'http://localhost:3001/';
-const END_POINT = 'https://ibump.herokuapp.com/chat';
+const END_POINT = 'https://ibump.herokuapp.com/';
 
 function App() {
   const [user, setUser] = useState(userService.getUser());
